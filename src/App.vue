c260740f08af4f8390255b4712d13ff82380c193
Tom Wong <3766639606@qq.com>
2018-10-27 22:12:27 +0800

    增加rem设置

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,20 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods:{
+    resizeFontSize(){
+      const docEl=document.documentElement;
+      const {clientWidth}=docEl;
+      if(!clientWidth)return;
+      //设计图以750px为准时，px rem比例为1：100
+      const fs=100*(clientWidth/750);
+      docEl.style.fontSize=`${fs}px`;
+    }
+  },
+  mounted() {
+    this.resizeFontSize();
+  },
 }
 </script>
 
