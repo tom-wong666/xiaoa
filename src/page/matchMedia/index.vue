a17071addc42fe3be1dad27bedd14f5df0584f8c
Tom Wong <3766639606@qq.com>
2018-11-16 18:48:01 +0800

    添加自定义响应式网页的demo

---
+++ src/page/matchMedia/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='media' @click="getTips">
+  window.matchMedia查询结果提示
+  </div>
+</template>
+
+<script>
+  export default{
+    methods:{
+      getTips(){
+        //点击时验证js提示色是否和如下css设置的一致
+        if(window.matchMedia('screen and (min-width:992px)').matches){
+          alert("大屏 红色")
+        }else if(window.matchMedia('screen and (min-width:768px) and (max-width: 992px)').matches){
+          alert("中屏 绿色")
+        }else if(window.matchMedia('screen and (max-width: 768px)').matches){
+          alert("小屏 蓝色")
+        }
+      },
+    },
+  }
+</script>
+
+<style lang="less" scoped>
+  @media  screen and (min-width:992px){
+    .media{
+      color:red;  
+    }
+  }
+  @media  screen and (min-width:768px) and (max-width: 992px){
+    .media{
+      color:green;  
+    }
+  }
+  @media  screen and (max-width: 768px){
+    .media{
+      color:blue;  
+    }
+  }    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/page/Home'
 import chart from '@/page/charts/index'
+import matchMedia from '@/page/matchMedia/index'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/chart',
       name: 'chart',
       component: chart
-    }    
+    },
+    {
+      path: '/matchMedia',
+      name: 'matchMedia',
+      component: matchMedia
+    }         
   ]
 })
